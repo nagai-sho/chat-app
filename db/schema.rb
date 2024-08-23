429664baf06ce59a52ad6e01dd47c099caa17b73
Nagai_sho <as.uland.dr@gmail.com>
2024-08-23 15:56:14 +0900

    usersテーブルの作成（失敗）
    name カラムが追加できない

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,7 @@
 
 ActiveRecord::Schema[7.0].define(version: 2024_08_22_202622) do
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
+    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
